8e540b11573d3be7b7f134018c46708f21e1fda6
Nobel Elan <nobelelan@gmail.com>
2023-02-18 17:24:05 +0600

    animation done

--- app/src/main/java/com/example/passwordmanagerassignment/ui/SplashActivity.kt
+++ app/src/main/java/com/example/passwordmanagerassignment/ui/SplashActivity.kt
@@ -6,14 +6,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.animation.AnimationUtils
 import com.example.passwordmanagerassignment.R
+import com.example.passwordmanagerassignment.databinding.ActivitySplashBinding
 import kotlinx.coroutines.delay
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val scaleAnim = AnimationUtils.loadAnimation(this, R.anim.splash_icon_anim)
+        binding.imageView.startAnimation(scaleAnim)
+
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
             finish()
